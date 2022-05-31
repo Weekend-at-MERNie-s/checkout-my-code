94a2d68ea756472ee9932037cc9ba225393196f9
geicibarham <geicianecosta43@gmail.com>
2022-05-31 14:33:18 -0400

    landing page is almost ready

--- client/src/components/landing-page/index.js
+++ client/src/components/landing-page/index.js
@@ -7,13 +7,13 @@ function LandingPage() {
         <>
             <section id="dog-container">
                 <div class="text-container">
-                    <h3 class="text">Have a working app but want to make < br />
+                    <h3 id="working-app" class="text">Have a working app but want to make < br />
                         your code better?
                     </h3>
                     <h4 class="text">Sign up and connect with other <br />
                         developers that can help!</h4>
                 </div>
-                <img style={{ height: "400px" }} src={dog} alt="cute dog with glasses" />
+                <img style={{ height: "400px" ,width:"500px"}} src={dog} alt="cute dog with glasses" />
             </section>
             <div>
                 < Footer />
