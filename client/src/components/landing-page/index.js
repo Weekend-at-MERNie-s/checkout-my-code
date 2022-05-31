f0d8639c6cde10f43fe9574997c70603e9b754ce
geicibarham <geicianecosta43@gmail.com>
2022-05-31 13:53:49 -0400

    fixing image and text align on landing page

--- client/src/components/landing-page/index.js
+++ client/src/components/landing-page/index.js
@@ -1,16 +1,31 @@
 import dog from '../../assets/images/dog-cartoon.png'
 import css from './landing.css'
+import Footer from '../footer';
 
 function LandingPage() {
     return (
         <>
             <section id="dog-container">
+                <div class="text-container">
+
+                    <h1 class="text">Codespect</h1>
+
+
+                    <h3 class="text">Have a working app but want to make < br />
+                        your code better?
+                    </h3>
+
+
+                    <h4 class="text">Sign up and connect with other <br />
+                        developers that can help!</h4>
+                </div>
                 <img style={{ height: "400px" }} src={dog} alt="cute dog with glasses" />
             </section>
+            <div>
+                < Footer />
+            </div>
         </>
     )
 }
-
-
 
 export default LandingPage;
