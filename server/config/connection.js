e2f1e4918858c583ceec88f76107654a770cef20
utilrr <gmarkghealy@gmail.com>
2022-05-31 23:07:13 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/checkout-my-code',
+  process.env.MONGODB_URI || "mongodb://localhost/checkout-my-code",
   {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
