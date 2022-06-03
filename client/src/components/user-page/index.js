f026b21b63a6574fd39e216c5d4041228603860d
dogmom3 <blair.seivers@yahoo.com>
2022-06-03 10:12:49 -0400

    post data display

--- client/src/components/user-page/index.js
+++ client/src/components/user-page/index.js
@@ -3,15 +3,14 @@ import css from "./style.css";
 import SinglePost from "../single-post";
 import dog from '../../assets/images/dog-cartoon.png'
 
-
 function UserPage() {
-
     const [title, setTitle] = useState('');
     const [github, setGithub]= useState('');
     const [deploy, setDeploy]= useState('');
     const [justify, setJustify]= useState('');
 
     const [postList, setPostList]= useState([]);
+
     const handleSubmit = (event)=> {
         event.preventDefault();
         console.log('title', title)
@@ -19,38 +18,50 @@ function UserPage() {
         console.log('deploy', deploy)
         console.log('justify', justify)
 
-        //await fetch.post (title, content)
+        // await fetch.post  (title, github, deploy, justify)
 
         // setTitle('');
-        // setContent('');
+        // setGithub('');
+        // setDeploy('');
+        // setJustify('');
     }
-    
-    // useEffect - grab data from the backend so you can display
-    // useEffect(() => {
-    //     let result = await fetch.get('url route')
-    //     setPostList(result)
-    //   }, []);
+   
+useEffect(() => {
+  posts()
+}, [])
+const posts = async ()=>{
+  const response = await fetch('user-page');
+  setPostList(await response.json())
+}
 
+    // useEffect(() => {
+    //     let result = await fetch.get('/user-page')
+    //     setPostList(await result.json())
+    //   }, []);
+      // if (response.ok) {
+      //   document.location.replace('/');
+      // } else {
+      //   alert(response.statusText);
+      // }
   return (
     <section className='user-post-page'>
-        <form className="post-form" onSubmit={handleSubmit}>
+      <form className="post-form" onSubmit={handleSubmit}>
           <h1>Create New Post</h1>
-          {/* <div className='input'> */}
             <div>
              <label htmlFor="post-title">Title:</label>
               <input className="form-input" type="text" id="post-title" name="post-title" onChange={e=> setTitle(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="github">GitHub Repo Link:</label>
-              <input className="form-input" id="content" name="content" onChange={e=> setGithub(e.target.value)}/>
+              <input className="form-input" id="github" name="github" onChange={e=> setGithub(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="deploy">Deployed App Link:</label>
-              <input className="form-input" id="content" name="content" onChange={e=> setDeploy(e.target.value)}/>
+              <input className="form-input" id="appLink" name="appLink" onChange={e=> setDeploy(e.target.value)}/>
             </div>
             <div>
               <label htmlFor="content">Justify my post:</label>
-              <textarea className="form-input" id="justify" name="content" onChange={e=> setJustify(e.target.value)}></textarea>
+              <textarea className="form-input" id="justify" name="justify" onChange={e=> setJustify(e.target.value)}></textarea>
              </div>
              <div className='merge'>
              <img id="dog" style={{ height: "85px", width: "85px" }} src={dog} alt="cute dog with glasses" />
@@ -59,14 +70,20 @@ function UserPage() {
                 Merge my code
                 </button>
             </div>
-        </form>
+      </form>
 
-      {/* </div> */}
-        <SinglePost />
+      <div className='postList'>
+        <h1>post list displays below</h1>
+        <ol className='list'>
+          {postList.map((data)=>{
+            return(
+              <li className='list-item' key={data.id}>{data.handleSubmit}</li>
+            )
+          })}
+        </ol>
+      </div>
+
         {/* display list post- need to use .map method (postList) */}
-        {/* <div>
-        <button className='btn'>EDIT POST</button>
-        </div> */}
     </section>
   );
 }
