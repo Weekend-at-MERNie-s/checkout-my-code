6d6c015f0c930c4f0a89b98928968a81346e1c1f
geicibarham <geicianecosta43@gmail.com>
2022-06-05 13:54:42 -0400

    fixed resolvers and still working on front end query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,19 +3,14 @@ import { gql } from '@apollo/client';
 export const QUERY_POSTS = gql`
   query posts($username: String) {
    posts(username: $username) {
-        _id: ID
-        title: String 
-        postContent: String
-        postRepoLink: String
-        deployedApplication: String
-        createdAt: String
-        username: String 
-       comments {
-        commentText
+        _id
+        title
+        postContent
+        postRepoLink
+        deployedApplication
         createdAt
         username
-       
-      }
+     
     }
   }
 `;
