f642e1419c3c68ab1a96c4abc1f269578fba13dc
JHessler <jhessler11@gmail.com>
2022-06-07 21:15:14 -0400

    donate page

---
+++ client/src/components/Donate/index.js
@@ -0,0 +1,9 @@
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+ 
+export default function App() {
+   return (
+       <PayPalScriptProvider options={{ "client-id": "test" }}>
+           <PayPalButtons style={{ layout: "horizontal" }} />
+       </PayPalScriptProvider>
+   );
+}
