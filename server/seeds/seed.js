3ce6c664f28a67bc7d30b09e8e625fe97ebb3d02
geicibarham <geicianecosta43@gmail.com>
2022-06-09 15:58:29 -0400

    deleted references to commentauthor

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,34 +1,34 @@
-const db = require('../config/connection');
-const { Comment, Post, User } = require('../models');
-const commentSeeds = require('./commentSeeds.json');
-const postSeeds = require('./postSeeds.json');
-const userSeeds = require('./userSeeds.json');
+// const db = require('../config/connection');
+// const { Comment, Post, User } = require('../models');
+// const commentSeeds = require('./commentSeeds.json');
+// const postSeeds = require('./postSeeds.json');
+// const userSeeds = require('./userSeeds.json');
 
-db.once('open', async () => {
-  try{
-    await Comment.deleteMany({});
-    await User.deleteMany({});
-    await Post.deleteMany({});
+// db.once('open', async () => {
+//   try{
+//     await Comment.deleteMany({});
+//     await User.deleteMany({});
+//     await Post.deleteMany({});
 
-    await User.create(userSeeds);
+//     await User.create(userSeeds);
 
-    for (let i = 0; i < commentSeeds.length; i++) {
-      const { _id, commentAuthor } = await Comment.create(commentSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: commentAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+//     for (let i = 0; i < commentSeeds.length; i++) {
+//       const { _id, commentAuthor } = await Comment.create(commentSeeds[i]);
+//       const user = await User.findOneAndUpdate(
+//         { username: commentAuthor },
+//         {
+//           $addToSet: {
+//             thoughts: _id,
+//           },
+//         }
+//       );
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
 
-  console.log('all done!');
-  process.exit(0);
-});
+//   console.log('all done!');
+//   process.exit(0);
+// });
   
